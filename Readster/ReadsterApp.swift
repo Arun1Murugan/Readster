2a2f4dbaa41326ab4678a57f0d093ccebb55f7f7
Arun <arun14072001@gmail.com>
2025-04-19 01:03:24 +0530

    Initial Commit

---
+++ Readster/ReadsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadsterApp.swift
+//  Readster
+//
+//  Created by Arun M on 19/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ReadsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
